c044ee9d7cdab3a2c2c1495ebb67f83fc4f86a9b
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-02 11:16:53 +0530

    Move bean configuration to App02GamingSpring and remove GameConfiguration
    Bean definitions for GamingConsole and GameRunner were moved from the deleted GameConfiguration class into App02GamingSpring, which is now annotated with @Configuration. The application context is updated to use App02GamingSpring for configuration.

--- learn-spring-2/src/main/java/com/springlearn/learn_spring/App02GamingSpring.java
+++ learn-spring-2/src/main/java/com/springlearn/learn_spring/App02GamingSpring.java
@@ -6,12 +6,34 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.springlearn.learn_spring.game.GameRunner;
 import com.springlearn.learn_spring.game.GamingConsole;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import com.springlearn.learn_spring.game.Mario;
+
+@Configuration
 public class App02GamingSpring {
+	
+	@Bean
+	public GamingConsole game()
+	{
+		var game = new Mario();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gamerunner(GamingConsole game)
+	{
+		var gamerunner = new GameRunner(game);
+		return gamerunner;
+	}
 
 	public static void main(String[] args) 
 	{
 	
-		try (var context = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+		try (var context = new AnnotationConfigApplicationContext(App02GamingSpring.class)) {
 			//context.getBean(GamingConsole.class).right();
 			context.getBean(GameRunner.class).run();
 		} catch (BeansException e) {
