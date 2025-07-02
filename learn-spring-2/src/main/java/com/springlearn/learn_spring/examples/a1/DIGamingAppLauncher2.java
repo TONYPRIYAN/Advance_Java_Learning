df4c4de596a9f32b78821cbbd76c54b5288f4dba
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-02 12:46:49 +0530

    Add BusinessClass with dependencies and print bean
    Introduced BusinessClass component with Depen1 and Depen2 dependencies using @Autowired. Updated main method to print the BusinessClass bean from the application context.

--- learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/a1/DIGamingAppLauncher2.java
+++ learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/a1/DIGamingAppLauncher2.java
@@ -2,9 +2,52 @@ package com.springlearn.learn_spring.examples.a1;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class BusinessClass
+{
+	@Autowired
+	Depen1 depen1;
+	
+	@Autowired
+	Depen2 depen2;
+	
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("Using");
+		sb.append(depen1);
+		sb.append("and");
+		sb.append(depen2);
+		return sb.toString();
+		
+	}
+	
+}
+
+@Component
+class Depen1
+{
+	
+}
+
+@Component
+class Depen2
+{
+	
+}
+
+
+
+
+
+
+
 
 @Configuration
 @ComponentScan
@@ -18,6 +61,8 @@ public class DIGamingAppLauncher2 {
 			
 			Arrays.stream(context.getBeanDefinitionNames())
 			.forEach(System.out::println);
+			
+			System.out.print(context.getBean(BusinessClass.class));
 		}
 		}
 		
