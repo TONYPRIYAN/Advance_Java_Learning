472bc621cd0ecd577bbe6d0b5151b524667f8082
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-03 11:49:35 +0530

    Add ClassA and ClassB components with dependency injection
    Introduced ClassA and ClassB as Spring components. ClassA receives ClassB via setter injection using @Autowired. Updated CDILauncher to retrieve and print the injected ClassB instance from ClassA.

--- learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/cdi/CDILauncher.java
+++ learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/cdi/CDILauncher.java
@@ -2,9 +2,49 @@ package com.springlearn.learn_spring.examples.cdi;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Component
+class ClassA
+{
+	ClassB cls;
+
+	
+
+	public ClassB getCls() {
+		return cls;
+	}
+
+	@Autowired
+	public void setCls(ClassB cls) 
+	{
+		this.cls = cls;
+	}
+	
+	
+}
+
+@Component
+class ClassB
+{
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
 
 @Configuration
 @ComponentScan
@@ -16,8 +56,12 @@ public class CDILauncher {
 		try (var context = new AnnotationConfigApplicationContext(CDILauncher.class)) {
 			//context.getBean(GamingConsole.class).right();
 			
-			Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
+//			Arrays.stream(context.getBeanDefinitionNames())
+//			.forEach(System.out::println);
+			
+			
+			System.out.print(context.getBean(ClassA.class).getCls());
+			
 		}
 		}
 		
