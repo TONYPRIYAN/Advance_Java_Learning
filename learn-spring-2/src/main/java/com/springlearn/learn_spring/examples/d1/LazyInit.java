683aec66a994614ff8945624644ea2e5ce93f761
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-03 10:47:08 +0530

    Add @Lazy annotation to ClassB component
    ClassB is now annotated with @Lazy, deferring its initialization until it is first needed. This can help improve startup performance and manage dependencies more efficiently.

--- learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/d1/LazyInit.java
+++ learn-spring-2/src/main/java/com/springlearn/learn_spring/examples/d1/LazyInit.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 
@@ -15,6 +16,7 @@ class ClassA
 }
 
 @Component
+@Lazy
 class ClassB
 {
 	ClassA classa;
