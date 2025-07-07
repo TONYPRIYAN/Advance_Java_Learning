bb3767421b819bfc3b277473510c0bd48f748c71
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-07 10:35:35 +0530

    Add /identities endpoint to IdentityController
    Introduced a @RequestMapping for '/identities' in IdentityController to expose the retrieve() method as a REST endpoint.

--- learn-springboot/src/main/java/com/springboot/learn_springboot/IdentityController.java
+++ learn-springboot/src/main/java/com/springboot/learn_springboot/IdentityController.java
@@ -3,12 +3,14 @@ package com.springboot.learn_springboot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IdentityController 
 {
 	
+	@RequestMapping("/identities")
 	public List<Identity> retrieve()
 	{
 		return Arrays.asList
