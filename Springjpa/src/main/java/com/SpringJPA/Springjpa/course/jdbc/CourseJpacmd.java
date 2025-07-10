783babd15d29a12fba4762cacdf452f002eec5b1
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-10 11:54:19 +0530

    Add JPA repository and update command class
    Introduced CourseJpaRepo for JPA-based data access and replaced the JDBC repository in the command class with the new JPA repository. Renamed CourseJdbccmd to CourseJpacmd and updated method calls to use JPA methods.

--- Springjpa/src/main/java/com/SpringJPA/Springjpa/course/jdbc/CourseJpacmd.java
+++ Springjpa/src/main/java/com/SpringJPA/Springjpa/course/jdbc/CourseJpacmd.java
@@ -5,13 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.SpringJPA.Springjpa.course.Course;
+import com.SpringJPA.Springjpa.course.jpa.CourseJpaRepo;
 
 
 @Component
-public class CourseJdbccmd implements CommandLineRunner
+public class CourseJpacmd implements CommandLineRunner
 {
 	@Autowired
-	CourseJdbcRepo repo;
+	CourseJpaRepo repo;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -21,7 +22,7 @@ public class CourseJdbccmd implements CommandLineRunner
 		repo.insert(new Course(3,"Apititude","Tony"));
 		
 		//repo.delete(3);
-		System.out.println(repo.findId(2));
+		System.out.println(repo.find(2));
 		
 	}
 
---
+++ Springjpa/src/main/java/com/SpringJPA/Springjpa/course/jpa/CourseJpaRepo.java
@@ -0,0 +1,38 @@
+package com.SpringJPA.Springjpa.course.jpa;
+
+import org.springframework.stereotype.Repository;
+
+import com.SpringJPA.Springjpa.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+
+@Repository
+@Transactional
+public class CourseJpaRepo 
+{
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(Course course)
+	{
+		em.merge(course);
+	}
+	
+	public Course find(long id)
+	{
+		return em.find(Course.class, em);
+	}
+	
+	public void delete(long id)
+	{
+		Course c =  em.find(Course.class, em);
+		em.remove(c);
+	}
+	
+	
+
+}
