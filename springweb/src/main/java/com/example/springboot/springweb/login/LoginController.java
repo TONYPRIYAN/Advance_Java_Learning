19e3663196268c4bc0571c0d34bde90979e255df
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-11 13:31:05 +0530

    Add basic login controller and JSP view
    Introduces LoginController with a /login mapping that returns the 'login' view. Adds a simple login.jsp page to display a login message.

---
+++ springweb/src/main/java/com/example/springboot/springweb/login/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.springweb.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController
+{
+	
+	@RequestMapping("login")
+	public String Login()
+	{
+		return "login";
+	}
+
+}
