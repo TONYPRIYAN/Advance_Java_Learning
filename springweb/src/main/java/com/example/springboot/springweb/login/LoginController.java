dc4546c5a4a5dd7eeba810058fb0f6dcbd4e2297
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-12 12:39:52 +0530

    Remove unused logger from LoginController
    Eliminated the SLF4J logger and related debug statement from LoginController as they were not being used. This simplifies the controller code.

--- springweb/src/main/java/com/example/springboot/springweb/login/LoginController.java
+++ springweb/src/main/java/com/example/springboot/springweb/login/LoginController.java
@@ -1,23 +1,20 @@
 package com.example.springboot.springweb.login;
 
-import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import org.slf4j.LoggerFactory;
-
 @Controller
 public class LoginController
 {
-	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping("login")
 	public String Login(@RequestParam String name,ModelMap map)
 	{
 		
 		map.put("name", name);
-		logger.debug("This is Logger printing");
+		
 		return "login";
 	}
 
