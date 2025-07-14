8d9fb328ae426026962ff829f8356a0e7f3f13e5
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-14 19:43:04 +0530

    Add welcome page and handle login POST request
    Implemented a POST handler in LoginController to process login form submissions and display a welcome page. Added welcome.jsp to show a personalized greeting using the submitted username.

--- springweb/src/main/java/com/example/springboot/springweb/login/LoginController.java
+++ springweb/src/main/java/com/example/springboot/springweb/login/LoginController.java
@@ -1,19 +1,27 @@
 package com.example.springboot.springweb.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController
 {
 
-	@RequestMapping("login")
+	@RequestMapping(value="login",method = RequestMethod.GET)
 	public String Login()
 	{
-		
-		
-		
 		return "login";
+	}
+	
+	@RequestMapping(value="login",method = RequestMethod.POST)
+	public String welcome(@RequestParam String name,@RequestParam String pwd,ModelMap map)
+	{
+		map.put("name",name);
+		map.put("pwd", pwd);
+		return "welcome";
 	}
 
 }
