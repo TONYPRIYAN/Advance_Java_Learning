08455a71e4f8bb0b98629bc27e74dac8075cb6a1
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-16 19:19:42 +0530

    Pass todo list to view using ModelMap
    Updated TodoController to add the todo list to the ModelMap and changed todos.jsp to display the 'todolist' attribute. This enables proper rendering of the todo items in the view.

--- springweb/src/main/java/com/example/springboot/springweb/todo/TodoController.java
+++ springweb/src/main/java/com/example/springboot/springweb/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.example.springboot.springweb.todo;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,9 +19,10 @@ public class TodoController
 	
 	
 	@RequestMapping("list-todos")
-	public String displayTodos()
+	public String displayTodos(ModelMap map)
 	{
 		List<Todo> todolist = todoserv.findByUsernames("Tony");
+		map.addAttribute("todolist", todolist);
 		return "todos";
 	}
 
