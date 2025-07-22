d358442ec6f5fdf405ee8242887c67f3557b286c
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-22 19:24:24 +0530

    Fix: Spring Security Configured

---
+++ springweb/src/main/java/com/example/springboot/springweb/security/SpringSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.springboot.springweb.security;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration 
+{
+	@Bean
+	public InMemoryUserDetailsManager createUserDetails()
+	{
+		
+		UserDetails userDetails = User.withUsername("Tony")
+		        .password(passwordEncoder().encode("Stark"))
+		        .roles("USER", "ADMIN")
+		        .build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+}
