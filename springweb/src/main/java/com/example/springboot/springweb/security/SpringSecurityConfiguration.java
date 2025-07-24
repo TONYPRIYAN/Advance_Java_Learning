dc053c64fe87577ee89edc9a22889626d673327b
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-24 19:18:51 +0530

    Add H2 in-memory database and update security config
    Configured H2 in-memory database in application.properties. Updated SpringSecurityConfiguration to use SecurityFilterChain, disable CSRF and frame options, and require authentication for all requests.

--- springweb/src/main/java/com/example/springboot/springweb/security/SpringSecurityConfiguration.java
+++ springweb/src/main/java/com/example/springboot/springweb/security/SpringSecurityConfiguration.java
@@ -2,12 +2,17 @@ package com.example.springboot.springweb.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -35,5 +40,19 @@ public class SpringSecurityConfiguration
 	public PasswordEncoder passwordEncoder()
 	{
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated()
+				);
+		http.formLogin(withDefaults());
+		http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+		return http.build();
+		
 	}
 }
