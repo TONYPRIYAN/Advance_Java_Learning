d78f68d62135d3a6bc197c3c03459cf9a1203c2b
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-24 19:37:24 +0530

    Add JPA entity mapping and database initialization
    Annotated Todo.java as a JPA entity and configured ID generation for persistence. Updated application.properties to defer datasource initialization for loading initial data. Added data.sql to seed the H2 database with sample todo entries.

---
+++ springweb/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID,USRNAME,DESC,TARGET,DONE)
+values(10001,'Bruce','Get SpringBoot',Current_date(), false);
+
+insert into todo (ID,USRNAME,DESC,TARGET,DONE)
+values(10002,'Bruce','Get AWS',Current_date(), false);
+
+insert into todo (ID,USRNAME,DESC,TARGET,DONE)
+values(10003,'Bruce','Get Machine Learning',Current_date(), false);
+
+insert into todo (ID,USRNAME,DESC,TARGET,DONE)
+values(10004,'Bruce','Get Cloud',Current_date(), false);
