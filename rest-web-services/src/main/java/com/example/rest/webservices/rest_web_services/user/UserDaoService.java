a01a117898b7e807c272ac37920a3476b343abb2
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-04 19:10:05 +0530

    Add UserResource REST controller and update UserDaoService
    Introduced UserResource to expose a /users endpoint returning all users. Updated UserDaoService to be a Spring component, added sample users, and provided a findAll() method for user retrieval.

--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/UserDaoService.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/UserDaoService.java
@@ -1,5 +1,28 @@
 package com.example.rest.webservices.rest_web_services.user;
 
-public class UserDaoService {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserDaoService 
+{
+	
+	private static List<User> users = new ArrayList<>();
+	
+	static
+	{
+		users.add(new User(1,"Alex",LocalDate.now().minusYears(27)));
+		users.add(new User(2,"Bob",LocalDate.now().minusYears(28)));
+		users.add(new User(3,"Carol",LocalDate.now().minusYears(29)));
+	}
+	
+	public List<User> findAll()
+	{
+		return users;
+	}
+			
 }
---
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/UserResource.java
@@ -0,0 +1,26 @@
+package com.example.rest.webservices.rest_web_services.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserResource 
+{
+	private UserDaoService service;
+
+	public UserResource(UserDaoService service) {
+		
+		this.service = service;
+	}
+	
+	@GetMapping("/users")
+	public List<User> retrieveall()
+	{
+		return service.findAll();
+	}
+	
+
+}
