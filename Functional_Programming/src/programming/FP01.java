ffc165094857473929b8ee8a11f0f37d68af2b17
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-05 19:17:43 +0530

    Refactor printall to use stream and method reference
    Replaced the for-each loop in printall with a stream and method reference to printnum for improved readability and functional programming style. Added printnum helper method for printing individual numbers.

--- Functional_Programming/src/programming/FP01.java
+++ Functional_Programming/src/programming/FP01.java
@@ -10,12 +10,22 @@ public class FP01 {
 
 	}
 	
+	public static void printnum(int num)
+	{
+		System.out.println(num);
+	}
+	
+	
+	
 	private static void printall(List<Integer> list)
 	{
-		for(int x : list)
-		{
-			System.out.println(x);
-		}
+//		for(int x : list)
+//		{
+//			System.out.println(x);
+//		}
+		
+		list.stream()
+				.forEach(FP01::printnum);
 		
 	}
 
