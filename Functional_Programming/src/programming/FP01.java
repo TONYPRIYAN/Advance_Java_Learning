dcc8242aec77ba1f3ab0552aab5e15d3057dd460
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-05 19:36:44 +0530

    Function pgm Practice

--- Functional_Programming/src/programming/FP01.java
+++ Functional_Programming/src/programming/FP01.java
@@ -7,8 +7,9 @@ public class FP01 {
 	public static void main(String[] args) {
 		
 		//printall(List.of(1,2,3,4,5,6,7));
-		printeven(List.of(1,2,3,4,5,6,7,8,9,10));
-
+		//printeven(List.of(1,2,3,4,5,6,7,8,9,10));
+		//specificCourse(List.of("Java","Python","API","Spring"));
+		CourseChars(List.of("Java","Python","API","Spring"));
 	}
 	
 //	private static boolean iseven(int num)
@@ -37,9 +38,22 @@ public class FP01 {
 	
 		
 		list.stream()
-				.filter(num -> num%2 == 0)
+				.filter(num -> num%2 > 0)
 				.forEach(System.out::println);
 		
+	}
+	
+	private static void specificCourse(List<String> list)
+	{
+		list.stream()
+			.filter(course -> course.equals("Spring")).
+			forEach(System.out::println);
+	}
+	private static void CourseChars(List<String> list)
+	{
+		list.stream()
+			.filter(course -> course.length() >= 4).
+			forEach(System.out::println);
 	}
 
 }
