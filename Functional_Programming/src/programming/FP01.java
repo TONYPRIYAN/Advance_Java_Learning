5d8341ab58bc4998a6e2a8d43eb5e135e0268db7
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-05 19:27:52 +0530

    Lambda  Expression

--- Functional_Programming/src/programming/FP01.java
+++ Functional_Programming/src/programming/FP01.java
@@ -6,26 +6,39 @@ public class FP01 {
 
 	public static void main(String[] args) {
 		
-		printall(List.of(1,2,3,4,5,6,7));
+		//printall(List.of(1,2,3,4,5,6,7));
+		printeven(List.of(1,2,3,4,5,6,7,8,9,10));
 
 	}
 	
-	public static void printnum(int num)
-	{
-		System.out.println(num);
-	}
+//	private static boolean iseven(int num)
+//	{
+//		return num % 2 == 0;
+//	}
+	
 	
 	
 	
 	private static void printall(List<Integer> list)
 	{
+		//Structured
 //		for(int x : list)
 //		{
 //			System.out.println(x);
 //		}
 		
 		list.stream()
-				.forEach(FP01::printnum);
+				.forEach(System.out::println);
+		
+	}
+	
+	private static void printeven(List<Integer> list)
+	{
+	
+		
+		list.stream()
+				.filter(num -> num%2 == 0)
+				.forEach(System.out::println);
 		
 	}
 
