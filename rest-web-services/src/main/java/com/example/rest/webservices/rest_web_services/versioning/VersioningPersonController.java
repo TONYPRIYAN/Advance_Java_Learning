b368ff60d552a5896ca782d105a3bcfc8b0e1307
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-20 12:16:01 +0530

    Add header-based API versioning for Person endpoint
    Introduced two new GET mappings for /person/header using the X-API-VERSION header to support version 1 and version 2 responses. This enables clients to request different versions of the Person resource via custom headers.

--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/versioning/VersioningPersonController.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/versioning/VersioningPersonController.java
@@ -34,6 +34,16 @@ public class VersioningPersonController
 		
 	}
 	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
 	
 	
 
