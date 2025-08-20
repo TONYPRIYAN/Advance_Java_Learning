26ca21a0748650fc8e00c1a192d9b57e48840b07
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-20 11:38:23 +0530

    I18n

--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/helloworld/HelloWorldController.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.example.rest.webservices.rest_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
+	private MessageSource msgsrc;
 	
+	
+	
+	public HelloWorldController(MessageSource msgsrc) {
+		
+		this.msgsrc = msgsrc;
+	}
+
 	@GetMapping(path="/hallo")
 	public String helloworld()
 	{
@@ -24,6 +36,13 @@ public class HelloWorldController
 	public HelloWorldBean helloworldPV(@PathVariable String name)
 	{
 		return new HelloWorldBean("Hallo "+name);
+	}
+	
+	@GetMapping(path="/hallo-i18n")
+	public String helloworldi18n()
+	{
+		Locale loc = LocaleContextHolder.getLocale();
+		return msgsrc.getMessage("good.morning.message",null,"Default Message",loc);
 	}
 
 	  
