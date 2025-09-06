0a31549a02b79fe891bdc10d0847ee6d472a9fc1
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-09-06 12:07:20 +0530

    Add JPA entity for User and seed initial data
    Annotated User class as a JPA entity and configured ID generation. Added Data.sql to seed initial user data. Updated application.properties to configure H2 database and enable JPA datasource initialization.

--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/User.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/User.java
@@ -4,14 +4,23 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+
+@Entity(name = "user_details")
 public class User 
 {
 	@Size(min=2,message="Name should min 2")
 	@JsonProperty("user_name")
 	private String name;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Past(message="should be past")
---
+++ rest-web-services/src/main/resources/Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_details (dob, name) VALUES ('1990-05-14', 'John');
+INSERT INTO user_details (dob, name) VALUES ('1985-11-20', 'Alice');
+INSERT INTO user_details (dob, name) VALUES ('1992-07-02', 'Robert');
+INSERT INTO user_details (dob, name) VALUES ('1978-03-29', 'Emma');
+INSERT INTO user_details (dob, name) VALUES ('2000-01-10', 'David');
+INSERT INTO user_details (dob, name) VALUES ('1995-09-18', 'Sophia');
