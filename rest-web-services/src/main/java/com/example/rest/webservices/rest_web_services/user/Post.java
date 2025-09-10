35087c6f3ed69b3435b39825d6c794cdece1f1cc
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-09-10 13:01:57 +0530

    Add JPA entity mapping for Post and User
    Post and User classes are now annotated as JPA entities. Added entity fields, relationships, and Jackson annotations to support persistence and serialization. Post is linked to User via @ManyToOne, and User has a @OneToMany list of posts.

--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/Post.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/Post.java
@@ -1,5 +1,51 @@
 package com.example.rest.webservices.rest_web_services.user;
 
-public class Post {
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post 
+{
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String desc;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", desc=" + desc + "]";
+	}
+	
+	
+	
+	
+
+	
 }
--- rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/User.java
+++ rest-web-services/src/main/java/com/example/rest/webservices/rest_web_services/user/User.java
@@ -1,13 +1,16 @@
 package com.example.rest.webservices.rest_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -31,6 +34,11 @@ public class User
 	@Past(message="should be past")
 	@JsonProperty("BirthDate")
 	private LocalDate dob;
+	
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	List<Post> user;
 	
 	
 	
