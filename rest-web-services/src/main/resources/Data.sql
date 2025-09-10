651d750dfdb64fc4a1ead81c886e1b3255efdb7e
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-09-10 13:16:56 +0530

    Add sample Post data to Data.sql
    Inserted initial Post records associated with user IDs to provide sample data for testing and development.

--- rest-web-services/src/main/resources/Data.sql
+++ rest-web-services/src/main/resources/Data.sql
@@ -4,3 +4,11 @@ INSERT INTO user_details (dob, name) VALUES ('1992-07-02', 'Robert');
 INSERT INTO user_details (dob, name) VALUES ('1978-03-29', 'Emma');
 INSERT INTO user_details (dob, name) VALUES ('2000-01-10', 'David');
 INSERT INTO user_details (dob, name) VALUES ('1995-09-18', 'Sophia');
+
+
+INSERT INTO Post (id, desc, user_id) VALUES (20001, 'Learning', 1);
+INSERT INTO Post (id, desc, user_id) VALUES (20002, 'Traveling to the mountains', 2);
+INSERT INTO Post (id, desc, user_id) VALUES (20003, 'Working on a new project', 3);
+INSERT INTO Post (id, desc, user_id) VALUES (20004, 'Excited about my birthday!', 4);
+INSERT INTO Post (id, desc, user_id) VALUES (20005, 'Reading a great book', 5);
+INSERT INTO Post (id, desc, user_id) VALUES (20006, 'Cooking something new', 6);
